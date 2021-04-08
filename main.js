08ef15ff7253b5b34b5d08da490c0669dd867798
hasansoftware <retiredlawyyer@gmail.com>
2021-04-09 01:51:00 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,125 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+const { ipcRenderer } = require('electron');
+
+const { app, BrowserWindow, Menu, ipcMain } = electron;
+
+let mainWindow, newWindow
+let todoList = []
+
+app.on("ready", () => {
+
+    mainWindow = new BrowserWindow({
+        webPreferences: {               // https://stackoverflow.com/a/55908510
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+        // frame: false
+    })
+
+    mainWindow.setResizable(false)
+
+    mainWindow.loadURL(
+        url.format({
+            pathname: path.join(__dirname, "pages/mainWindow.html"),
+            protocol: "file:",
+            slashes: true
+        })
+    )
+
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
+    Menu.setApplicationMenu(mainMenu)
+
+    ipcMain.on("key:newTodoClose", () => {
+        newWindow.close()
+        newWindow = null
+    })
+
+    ipcMain.on("key:newTodoSave", (err, data) => {
+        todoList.push({
+            id: todoList.length +1,
+            value: data
+        })
+        console.log(todoList);
+        newWindow.close()
+        newWindow = null
+    })
+
+    mainWindow.on("close", () => {
+        app.quit()
+    })
+})
+
+const mainMenuTemplate = [
+    {
+        label: "Dosya",
+        submenu: [
+            {
+                label: "Yeni ekle",
+                click() {
+                    createWindow()
+                }
+            },
+            {
+                label: "Tümünü sil"
+            },
+            {
+                label: "Çıkış",
+                role: "quit",
+                accelerator: process.platform == "darwin" ? "Command+q" : "Ctrl+q"
+            }
+        ]
+    }
+]
+
+if (process.platform == "darwin") {
+    mainMenuTemplate.unshift({
+        label: app.getName(),
+        role: "TODO"
+    })
+}
+
+if (process.env.NODE_ENV !== "production") {
+    mainMenuTemplate.push({
+        label: "Dev Tools",
+        submenu: [
+            {
+                label: "Geliştirici Penceresini Aç / Kapat",
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools()
+                }
+            },
+            {
+                label: "Yenile",
+                role: "reload"
+            }
+        ]
+    })
+}
+
+
+function createWindow() {
+    newWindow = new BrowserWindow({
+        webPreferences: {               // https://stackoverflow.com/a/55908510
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+        width: 472,
+        height: 172,
+        title: "Yeni TODO Ekle",
+        frame: false
+    })
+    newWindow.setResizable(false)
+    newWindow.loadURL(
+        url.format({
+            pathname: path.join(__dirname, "pages/newTodo.html"),
+            protocol: "file:",
+            slashes: true
+        })
+    )
+
+    newWindow.on("close", () => {
+        newWindow = null
+    })
+}
